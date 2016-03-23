cb7ef1500dc305f209fdae454b34575f48a4bbd7
josecmendoza <jose.mendoza012@yahoo.com>
2016-03-23 15:37:34 -0500

    Added first project files

---
+++ js/js/app.js
@@ -0,0 +1,22 @@
+//Problem: Search filter for photo does not work
+//Solution: When a user types in a key word into the search bar,
+//have the photos filter out according to the key words in real time.
+
+//Attach search bar to the header
+  var $search = $("<input>").attr({"class": "filter_search","type": "text","placeholder": "Search"});
+  $('.header').append($search);
+
+    $search.keyup(function() {
+
+      var filter = $(this).val();
+
+      $("#gallery li a").each(function() {
+        if ($(this).attr("alt" && "data-title").search(new RegExp(filter, "i")) < 0) {
+          $(this).fadeOut();
+        } else {
+            $(this).fadeIn();
+        }
+
+      });
+
+    });
